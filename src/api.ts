95beb27aac1b759f3eeb314bde0be5b0e6a8460d
Jan Erik Hanssen <jhanssen@gmail.com>
2020-02-02 14:54:09 -0800

    api interface

---
+++ src/api.ts
@@ -0,0 +1,8 @@
+import { EnvType } from "./variable";
+import { SubshellResult } from "./subshell";
+
+export interface API {
+    declare(name: string, func: (args: string[], env: EnvType) => Promise<number | undefined>): void;
+    export(name: string, value: string | undefined): void;
+    run(cmdline: string): Promise<SubshellResult>;
+}
